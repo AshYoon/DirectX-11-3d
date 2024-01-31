3f8dc1a761a0e6d1238975696333d1848a7116aa
AshYoon <rlawodbs7691@naver.com>
2024-02-01 01:26:23 +0900

    simplemath

--- 23_12_28/GameCoding/GameCoding/Types.h
+++ 23_12_28/GameCoding/GameCoding/Types.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <Windows.h>
+#include "SimpleMath.h"
 #include "DirectXMath.h"
 
 using int8 = __int8;
@@ -11,7 +13,13 @@ using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
 
 // vector Types 
-using Vec2 = DirectX::XMFLOAT2;
-using Vec3 = DirectX::XMFLOAT3;
-using Vec4 = DirectX::XMFLOAT4;
-using Color = DirectX::XMFLOAT4;
+//using Vec2 = DirectX::XMFLOAT2;
+//using Vec3 = DirectX::XMFLOAT3;
+//using Vec4 = DirectX::XMFLOAT4;
+//using Color = DirectX::XMFLOAT4;
+
+//direct x simple math 의 vector를 가져온다 ( 마소에서 제공한 ) 
+using Vec2 = DirectX::SimpleMath::Vector2;
+using Vec3 = DirectX::SimpleMath::Vector3;
+using Vec4 = DirectX::SimpleMath::Vector4;
+using Matrix = DirectX::SimpleMath::Matrix;
